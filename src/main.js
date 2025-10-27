0368490622b13b5bd6873ac09fce5a8375f696d3
ki8yk8 <k***@gmail.com>
2025-10-27 22:10:31 +0545

    creates some walls

--- src/main.js
+++ src/main.js
@@ -5,6 +5,17 @@ const k = kaplay({ global: false });
 
 k.loadRoot("./"); // A good idea for Itch.io publishing later
 
-registerMapScene({ k, name: "map" });
+// defining constants
+const CELL_SIZE = 32;
+const WALL_COLOR = "#000000";
+
+registerMapScene({
+	k,
+	name: "map",
+	constants: {
+		CELL_SIZE,
+		WALL_COLOR,
+	},
+});
 
 k.go("map");
--- src/scenes/map.js
+++ src/scenes/map.js
@@ -1,13 +1,39 @@
-export function registerMapScene({ k, name }) {
+export function registerMapScene({ k, name, constants }) {
+	const CELL_SIZE = constants.CELL_SIZE;
+	const WALL_COLOR = constants.WALL_COLOR;
+
+	function rem(n, base = CELL_SIZE) {
+		return n * base;
+	}
+
 	k.scene(name, () => {
-		k.onDraw(() => {
-			k.drawRect({
-				width: k.width(),
-				height: k.height(),
-				pos: k.vec2(0, 0),
-				anchor: "topleft",
-				color: k.Color.fromHex("#ffffff"),
-			});
-		});
+		// creating the map borders
+		k.add([
+			k.rect(rem(0.5), rem(18)),
+			k.pos(30, k.height() - rem(18) - rem(1)),
+			k.color(WALL_COLOR),
+			"wall",
+		]);
+
+		k.add([
+			k.rect(rem(60), rem(0.5)),
+			k.pos(30, k.height() - rem(18) - rem(1)),
+			k.color(WALL_COLOR),
+			"wall",
+		]);
+
+		k.add([
+			k.rect(rem(70), rem(0.5)),
+			k.pos(30, k.height() - rem(1)),
+			k.color(WALL_COLOR),
+			"wall",
+		]);
+
+		k.add([
+			k.rect(rem(0.5), rem(18)),
+			k.pos(30 + rem(60), k.height() - rem(18) - rem(0.5)),
+			k.color(WALL_COLOR),
+			k.anchor("bot"),
+		]);
 	});
 }
