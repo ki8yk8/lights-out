b230cf3dbd788f2eee00c04d627c0dd3da6fd68b
ki8yk8 <k***@gmail.com>
2025-11-02 12:57:23 +0545

    adds the fuse drop text

--- src/objects/electrical-box.js
+++ src/objects/electrical-box.js
@@ -10,12 +10,24 @@ export function ElectricalBox({ k, c }) {
 		"electrical-box",
 		{
 			add() {
+				this.add([
+					k.text(`${k.data.fuse_dropped}/${k.data.fuse_needed}`),
+					k.color("#000000"),
+					k.pos(this.width / 2, this.height / 2),
+					k.anchor("center"),
+					"message",
+				]);
+
 				this.onCollide("player", () => {
 					if (k.data.fuse_held === 0) {
 						Message({ k, c, text: "Collect the fuses and drop them here" });
 					} else {
 						k.data.fuse_dropped += k.data.fuse_held;
 						k.data.fuse_held = 0;
+
+						this.get(
+							"message"
+						)[0].text = `${k.data.fuse_dropped}/${k.data.fuse_needed}`;
 					}
 				});
 			},
