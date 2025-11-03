37eacf006d2935b38097bcf969089436a698c2b5
ki8yk8 <k***@gmail.com>
2025-11-03 08:21:55 +0545

    change ghost to simpler ones because prvious one was laggy

--- src/objects/ghost.js
+++ src/objects/ghost.js
@@ -1,105 +1,23 @@
-// Spawning = randomly spawns the ghost at random points every x seconds or so for x seconds
-// Ghost movement = randomly moves and if human moves or does any action it hears the sound and follows toward it with speed = speed of human
-// Human die = if ghost sees the human then, human gets locked in and ghost runs with double the speed, if ghost is able to catch it human dies
-
 import { rem } from "../helpers/utils";
 
-// Exception: might be hidden space where human can sit and escape ghost
-export function Ghost({ k, c, pos }) {
-	const ghost = k.add([
+// new ghosts = & moves horizontally; first towards left
+// new ghosts = % moves vertically; fist move towards up
+
+export function Ghost({ k, c, horizontal = true }) {
+	return [
 		k.sprite("pumpkin"),
-		k.anchor("center"),
 		k.area(),
 		k.body(),
-		k.pos(pos),
-		k.z(1),
-		{
-			dir: {
-				x: 1,
-				y: 0,
-			},
-			stuck_frames: 0,
-			prev_pos: pos.clone(),
-			player_last_pos: null,
-		},
+		k.layer("game"),
+		k.scale(0.8),
 		"ghost",
-	]);
-
-	k.play("ghost-appear");
-
-	// move the ghost
-	ghost.vel.x = rem(1) * c.GHOST_SPEED * 0.5 * ghost.dir.x;
-	ghost.vel.y = rem(1) * c.GHOST_SPEED * 0.5 * ghost.dir.y;
-
-	// every frame ghost moves in one direction straight
-	ghost.onUpdate(() => {
-		const player = k.get("player", { recursive: true })[0];
-		if (player && !ghost.player_last_pos) {
-			ghost.player_last_pos = player.pos;
-		}
-
-		// if the player moved
-		if (player && player.pos.dist(ghost.player_last_pos) > 10) {
-			ghost.player_last_pos = player.pos;
-
-			// increase speed and run toward the player
-			const [dx, dy] = [player.pos.x - ghost.pos.x, player.pos.y - ghost.pos.y];
-			const distance = player.pos.dist(ghost.pos);
-
-			const [vx, vy] = [
-				(dx / distance) * c.GHOST_SPEED * rem(1),
-				(dy / distance) * c.GHOST_SPEED * rem(1),
-			];
-
-			ghost.vel.x = vx;
-			ghost.vel.y = vy;
-		}
-
-		if (ghost.pos.dist(ghost.prev_pos) < 10) {
-			ghost.stuck_frames += 1;
-		} else {
-			ghost.stuck_frames = 0;
-			ghost.prev_pos = ghost.pos.clone();
-		}
-
-		if (ghost.stuck_frames > 60) {
-			setRandomDirection();
-			ghost.stuck_frames = 0;
-		}
-	});
-
-	function setRandomDirection() {
-		const filtered_configs = configs.filter(([x, y]) => {
-			if (ghost.dir.x === x && ghost.dir.y === y) {
-				return false;
-			}
-			return true;
-		});
-
-		const config =
-			filtered_configs[Math.floor(Math.random() * filtered_configs.length)];
-		ghost.dir.x = config[0];
-		ghost.dir.y = config[1];
-		// move the ghost
-		ghost.vel.x = rem(1) * c.GHOST_SPEED * 0.5 * ghost.dir.x;
-		ghost.vel.y = rem(1) * c.GHOST_SPEED * 0.5 * ghost.dir.y;
-	}
-
-	// when collide on wall randomly change orientation
-	const configs = [
-		[0, 1],
-		[0, -1],
-		[1, 0],
-		[-1, 0],
+		{
+			add() {
+				// add logic to move horizontally up and down
+				// add sound effects
+				// add hit
+				// add hit sound
+			},
+		},
 	];
-
-	ghost.onCollide("wall", () => {
-		setRandomDirection();
-	});
-
-	ghost.onCollide("player", () => {
-		k.data.life--;
-	});
-
-	return ghost;
 }
--- src/objects/map.js
+++ src/objects/map.js
@@ -5,58 +5,6 @@ import { Fuse } from "./fuse";
 import { Ghost } from "./ghost";
 import { Hints } from "./hints";
 
-function transposeMatrix(mat) {
-	return mat[0].map((_, colIndex) => mat.map((row) => row[colIndex]));
-}
-
-function getSurroundingIndices(x, y, max_cols, max_rows, radius = 1) {
-	const surrounding_indices = [
-		[x - 1, y],
-		[x - 1, y - 1],
-		[x, y - 1],
-		[x + 1, y - 1],
-		[x + 1, y],
-		[x + 1, y + 1],
-		[x, y + 1],
-		[x - 1, y + 1],
-	];
-	const valid_neighbours = surrounding_indices.filter(([i, j]) => {
-		return i >= 1 && j >= 1 && i < max_cols && j < max_rows;
-	});
-
-	if (radius <= 1) {
-		return valid_neighbours;
-	}
-
-	let all_neighbours = [...valid_neighbours];
-	for (const [i, j] of valid_neighbours) {
-		all_neighbours = [
-			...all_neighbours,
-			...getSurroundingIndices(i, j, max_cols, max_rows, radius - 1),
-		];
-	}
-
-	return all_neighbours;
-}
-
-function getValidAreas(map_layout) {
-	// '=' = wall, " " = donot use, "V" = valid
-	let valid_area = map_layout.map((row) => {
-		const [first, last] = [row.indexOf("="), row.lastIndexOf("=")];
-
-		return row.map((elem, index) => {
-			if (elem === "=") return "=";
-			if (index > first && index < last && elem === " ") {
-				// inside the two limits of wall
-				return "V";
-			}
-			return " ";
-		});
-	});
-
-	return valid_area;
-}
-
 export function Map({ k, c, level }) {
 	const [tile_w, tile_ht] = [rem(1), rem(1)];
 	const [wall_w, wall_ht] = [rem(1), rem[1]];
@@ -72,93 +20,6 @@ export function Map({ k, c, level }) {
 		k.fixed(),
 		k.layer("bg"),
 	]);
-
-	function getRandomPosInsideWall() {
-		const arrayed_map_layout = map_layout.map((row) => row.split(""));
-		const horizontally_valid = getValidAreas(arrayed_map_layout);
-		const vertically_valid = transposeMatrix(
-			getValidAreas(transposeMatrix(arrayed_map_layout))
-		);
-
-		// full valid = valid in both
-		let fully_valid = [...arrayed_map_layout];
-		fully_valid = fully_valid.map((row, i) => {
-			return row.map((elem, j) => {
-				if (
-					horizontally_valid[i][j] === "V" &&
-					vertically_valid[i][j] === "V"
-				) {
-					return "V";
-				} else {
-					return " ";
-				}
-			});
-		});
-
-		// if the surroundign is also valid then, only valid
-		fully_valid = fully_valid.map((row, j) => {
-			return row.map((elem, i) => {
-				if (elem !== "V") return elem;
-
-				const neighbours = getSurroundingIndices(
-					i,
-					j,
-					fully_valid.length,
-					fully_valid[0].length,
-					2
-				);
-
-				const neighbours_valid = neighbours.every(([x, y]) => {
-					return fully_valid[y][x] === "V";
-				});
-
-				return neighbours_valid ? "V" : " ";
-			});
-		});
-
-		const flattened_valid = fully_valid.flat();
-		const total_valids = flattened_valid.reduce((acc, elem) => {
-			if (elem === "V") {
-				return acc + 1;
-			}
-			return acc;
-		}, 0);
-
-		const random_number = Math.floor(Math.random() * total_valids);
-
-		const valid_indices = [];
-		flattened_valid.forEach((item, index) => {
-			if (item === "V") valid_indices.push(index);
-		});
-
-		const flattened_index = valid_indices[random_number];
-
-		// if (flattened_valid[flattened_index] !== "V") {
-		// 	throw new Error("Flattened wrong");
-		// }
-
-		const [y, x] = [
-			Math.floor(flattened_index / arrayed_map_layout[0].length),
-			flattened_index % arrayed_map_layout[0].length,
-		];
-
-		// if (fully_valid[y][x] !== "V") {
-		// 	throw Error(index);
-		// }
-
-		return k.vec2(x * tile_w + rem(1), y * tile_ht + rem(1));
-	}
-
-	// spawning the ghosts
-	let first = true;
-	k.loop(k.data.ghost_interval, () => {
-		if (first) {
-			first = false;
-			return;
-		}
-		let ghost = Ghost({ k, c, pos: getRandomPosInsideWall() });
-		k.wait(k.data.ghost_lasts, () => k.destroy(ghost));
-	});
 
 	k.addLevel(map_layout, {
 		tileWidth: tile_w,
@@ -182,6 +43,8 @@ export function Map({ k, c, level }) {
 				k.layer("game"),
 				"player",
 			],
+			"&": Ghost.bind(null, { k, c, horizontal: true }),
+			"%": Ghost.bind(null, { k, c, horizontal: false }),
 			$: Fuse.bind(null, { k, c }),
 			"?": Hints.bind(null, { k, c }),
 			"#": ElectricalBox.bind(null, { k, c }),
